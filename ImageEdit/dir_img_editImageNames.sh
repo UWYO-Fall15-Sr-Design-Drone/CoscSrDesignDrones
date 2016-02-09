e782d41b57cac8e1f63ca768ed5c05d105ff43a4
Scott Efird <efirds@hotmail.com>
2016-02-09 16:35:22 -0700

    Making it harmless 💪💪💪
    Odds are we won't need this script.

--- ImageEdit/dir_img_editImageNames.sh
+++ ImageEdit/dir_img_editImageNames.sh
@@ -1,23 +1,23 @@
-#!/bin/bash
-#Scott Efird, last updated Feb 8 2016
-
-#This will open a dir, hunt for all the images and then change
-#their names into the format Photo_#.jpg
-
-clear
-#The directory we are looking in
-DIR="/home/waffles/git-repos/CoscSrDesignDrones/ImageEdit/Images"
-
-#Moving to our dir
-cd $DIR
-
-#Looping through the dir
-for f in *.jpg; do
-	j=
-
-	#http://unix.stackexchange.com/questions/6998/how-can-i-rename-photos-given-the-exif-data
-
-
-	#look into jhead to scrape data
-#Dumping us back to our home directory
-cd ..
+##!/bin/bash
+##Scott Efird, last updated Feb 8 2016
+#
+##This will open a dir, hunt for all the images and then change
+##their names into the format Photo_#.jpg
+#
+#clear
+##The directory we are looking in
+#DIR="/home/waffles/git-repos/CoscSrDesignDrones/ImageEdit/Images"
+#
+##Moving to our dir
+#cd $DIR
+#
+##Looping through the dir
+#for f in *.jpg; do
+#	j=
+#
+#	#http://unix.stackexchange.com/questions/6998/how-can-i-rename-photos-given-the-exif-data
+#
+#
+#	#look into jhead to scrape data
+##Dumping us back to our home directory
+#cd ..
