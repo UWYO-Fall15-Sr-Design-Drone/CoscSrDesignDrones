c05f3b98dcba4161d1aa4a000cc90c548d87afe8
scottefird <efirds@hotmail.com>
2016-02-09 17:10:42 -0700

    😻😻😻😻

--- ImageEdit/dir_img_EXIF_Scraper.sh
+++ ImageEdit/dir_img_EXIF_Scraper.sh
@@ -1,23 +1,23 @@
-##!/bin/bash
-##Scott Efird, last updated Feb 8 2016
-#
-##This will open a dir, hunt for all the images and then change
-##their names into the format Photo_#.jpg
-#
-#clear
-##The directory we are looking in
-#DIR="/home/waffles/git-repos/CoscSrDesignDrones/ImageEdit/Images"
-#
-##Moving to our dir
-#cd $DIR
-#
-##Looping through the dir
-#for f in *.jpg; do
-#	j=
-#
-#	#http://unix.stackexchange.com/questions/6998/how-can-i-rename-photos-given-the-exif-data
-#
-#
-#	#look into jhead to scrape data
-##Dumping us back to our home directory
-#cd ..
+!/bin/bash
+#Scott Efird, last updated Feb 8 2016
+
+#This will open a dir, hunt for all the images and then 
+#output all the exif data into a text file. 
+
+clear
+#The directory we are looking in
+DIR="/home/waffles/git-repos/CoscSrDesignDrones/ImageEdit/Images"
+
+#Moving to our dir
+cd $DIR
+
+#Looping through the dir
+for f in *.jpg; do
+	
+
+	#TODO
+	#Add if statement to check if image.
+	identify -verbose f | grep "exif:"
+done
+#Dumping us back to our home directory
+cd ..
