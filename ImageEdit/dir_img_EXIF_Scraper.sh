48e75e9f1c2e9f4c1df3574e3ef882567ee2ea7c
scottefird <efirds@hotmail.com>
2016-02-12 11:33:07 -0700

    Moved some files for ease of use. Started ASP framework

--- ImageEdit/dir_img_EXIF_Scraper.sh
+++ ImageEdit/dir_img_EXIF_Scraper.sh
@@ -7,16 +7,31 @@
 clear
 #The directory we are looking in
 DIR="/home/waffles/git-repos/CoscSrDesignDrones/ImageEdit/Images"
-
+FILE="exif_data.txt"
 #Moving to our dir
+
 cd $DIR
+
+if [ -f $FILE ]; then
+	echo "---------------------------------------------"
+	echo "~/git-repos/.../exif_data.txt already exists."
+	echo "Removing old exif_data.txt"
+	echo "---------------------------------------------"
+	rm $FILE
+fi
+
+
 
 #Looping through the dir
 for f in *.jpg; do
-	
-	#TODO
+ 	#TODO
 	#Add if statement to check if image.
-	identify -verbose f | grep "exif:"
+	echo "============================================="
+	echo "File: $f" >> $FILE
+	echo "---------------------------------------------"
+	string=$(echo identify -format %[exif:*] $f) 
+	echo string >> $FILE
+	echo "============================================="
 done
 #Dumping us back to our home directory
 cd ..
