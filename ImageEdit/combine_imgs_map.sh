7e7d36855c228beb02de3bb418620a84d1652dcd
root <root@chirr.uwyo.edu>
2016-03-29 16:56:46 -0600

    Saving work

--- ImageEdit/combine_imgs_map.sh
+++ ImageEdit/combine_imgs_map.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 #Scott Efird, last updated Feb 8 2016
 
-#this script will combine our images that come from the phone in the pattern
-#
+##This script will combine our images into a single master images
+#It will map to the pattern set in the read me doc. 
+
+clear
+
+#The directory we are looking in
+DIR="/home/waffles/git-repos/CoscSrDesignDrones/ImageEdit/Images"
+
+
+#First we will loop through the dir looking for images that match the pattern
